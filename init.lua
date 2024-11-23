91b72978047b2a0ade9cfd303b6b6d91926015c4
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-11-24 07:27:52 +0900

    Add <leader>pv

--- init.lua
+++ init.lua
@@ -97,6 +97,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- useful taken from primeagen videos
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set('n', '<leader>Y', [["+Y]])
 
